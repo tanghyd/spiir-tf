421c14aeef7dabc4785079d0242cff57ed258ad8
Daniel Tang <tang.d@hotmail.com>
2022-05-29 20:35:25 +0800

    Finish example script for MirroredStrategy

--- share/examples/mnist/train.py
+++ share/examples/mnist/train.py
@@ -1,22 +1,20 @@
-# internal modules
-import os
+import argparse
 import datetime
 import logging
+import os
+import time
+from pathlib import Path
+from typing import Dict, Optional, Tuple, Union
 
 # https://stackoverflow.com/a/38645250
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-
-BUFFER_SIZE = 10000
-BATCH_SIZE_PER_REPLICA = 64
-DISTRIBUTED = True
-
-
 # initialise logging
-logger = logging.getLogger("train_example")
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
@@ -27,16 +25,32 @@ console_log.setFormatter(formatter)
 logger.addHandler(console_log)
 
 
+def convert_loglevel(loglevel: int) -> int:
+    """Keras fit verbose:
+
+    verbose=0 will show you nothing (silent)
+    verbose=1 will show you an animated progress bar
+    verbose=2 will just print the number of epochs every line:
+
+    """
+    if args.loglevel <= logging.DEBUG:
+        verbose = 2
+    elif args.loglevel <= logging.INFO:
+        verbose = 1
+    else:
+        verbose = 0
+    return verbose
+
+
 def scale(image, label):
     image = tf.cast(image, tf.float32)
     image /= 255
-
     return image, label
 
 
 # Define a function for decaying the learning rate.
 # You can define any decay function you need.
-def decay(epoch):
+def decay(epoch: int) -> float:
     if epoch < 3:
         return 1e-3
     elif epoch >= 3 and epoch < 7:
@@ -45,19 +59,37 @@ def decay(epoch):
         return 1e-5
 
 
-def create_dataset(batch_size_per_replica: int, datasets, strategy):
-    # process, shuffle, and batch training data
-    batch_size = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
-    train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-    eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-    return train_dataset, eval_dataset
+def create_dataset(
+    batch_size_per_replica: int,
+    datasets: Dict[str, tf.data.Dataset],
+    strategy: Optional[tf.distribute.Strategy],
+    buffer: int = 10000,
+) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    # determine batch size
+    num_replicas = strategy.num_replicas_in_sync if strategy is not None else 1
+    batch_size = batch_size_per_replica * num_replicas
 
-def create_model(num_classes: int=10):
+    # process, shuffle, and batch training data
+    train_data = (
+        datasets["train"]
+        .map(scale)
+        .cache()
+        .shuffle(buffer)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+    val_data = (
+        datasets["test"].map(scale).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    )
+    return train_data, val_data
+
+
+def create_model(num_classes: int = 10):
     return tf.keras.Sequential([
-        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(32, 3, activation="relu", input_shape=(28, 28, 1)),
         tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation="relu"),
         tf.keras.layers.Dense(num_classes)
     ])
 
@@ -65,59 +97,77 @@ def create_model(num_classes: int=10):
 def train(
     n_epochs: int,
     num_classes: int,
-    train_data,
-    val_data,
-    strategy
+    train_data: tf.data.Dataset,
+    val_data: tf.data.Dataset,
+    strategy: Optional[tf.distribute.Strategy],
+    verbose: Union[int, bool],
 ):
     # enter tensorflow distributed strategy
-    if DISTRIBUTED:
+    if strategy is not None:
         with strategy.scope():
-            model = get_model(num_classes)
+            model = create_model(num_classes)
             model.compile(
                 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 optimizer=tf.keras.optimizers.Adam(),
-                metrics=['accuracy']
+                metrics=["accuracy"],
             )
     else:
-        model = get_cnn()
+        model = create_model(num_classes)
         model.compile(
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             optimizer=tf.keras.optimizers.Adam(),
-            metrics=['accuracy']
+            metrics=["accuracy"],
         )
 
     # Define the checkpoint directory to store the checkpoints.
-    checkpoint_dir = './training_checkpoints'
+    checkpoint_dir = "./training_checkpoints"
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
-
     callbacks = [
-        tf.keras.callbacks.TensorBoard(log_dir='./logs'),
-        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True),
+        # tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+        tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_prefix, save_weights_only=True
+        ),
         tf.keras.callbacks.LearningRateScheduler(decay),
-        PrintLR()
     ]
 
-    logger.info(f"Training model for {n_epochs} epochs")
-    model.fit(train_dataset, epochs=n_epochs, callbacks=callbacks)
-
-
-# Define a callback for printing the learning rate at the end of each epoch.
-class PrintLR(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        print(f'\nLearning rate for epoch {epoch + 1} is {model.optimizer.lr.numpy()}')
+    history = model.fit(
+        train_data,
+        epochs=n_epochs,
+        callbacks=callbacks,
+        validation_data=val_data,
+        verbose=verbose,
+    )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Train a Keras model."
-    )
+    parser = argparse.ArgumentParser(description="Train a Keras model.")
     parser.add_argument(
         "-n",
-        "--n_epochs",
+        "--n-epochs",
         type=int,
         default=12,
+        metavar="N",
         help="Number of training epochs",
+    )
+    parser.add_argument(
+        "--batch-size-per-replica",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size per GPU for training (default: 64)",
+    )
+    parser.add_argument(
+        "--distribute",
+        action="store_true",
+        dest="distribute",
+        help="Run distributed TensorFlow (default: False)",
+    )
+    parser.add_argument(
+        "--data-dir",
+        type=Path,
+        default=Path(__file__).parent / "data",
+        help="Specify data directory to download data set.",
     )
     parser.add_argument(
         "-d",
@@ -136,33 +186,43 @@ if __name__ == "__main__":
         const=logging.INFO,
         help="Set logging level to INFO and display progress and information",
     )
-    # parse command line arguments
     args = parser.parse_args()
-    c_log.setLevel(level=args.loglevel)  # logging console handler
+    console_log.setLevel(level=args.loglevel)  # logging console handler
 
+    start_time = time.perf_counter()
+
+    # check input types
     assert isinstance(args.n_epochs, int) and args.n_epochs > 0
+    assert (
+        isinstance(args.batch_size_per_replica, int) and args.batch_size_per_replica > 0
+    )
 
     # check available GPU devices
     logger.debug("Available devices:")
     for i, device in enumerate(tf.config.list_logical_devices()):
         logger.debug(f"({i}) {device}")
 
-    if DISTRIBUTED:
-        # define distribution strategy for multiple workers
-        strategy = tf.distribute.MirroredStrategy()
-        batch_size = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
-        logger.info(f'Number of devices: {strategy.num_replicas_in_sync}')
-    else:
-        strategy = None
-        batch_size = BATCH_SIZE_PER_REPLICA
-        logger.info("DISTRIBUTED=FALSE")
+    # define distribution strategy for multiple workers
+    strategy = tf.distribute.MirroredStrategy() if args.distribute else None
 
     # download dataset
-    datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+    datasets, info = tfds.load(
+        name="mnist", with_info=True, as_supervised=True, data_dir=args.data_dir
+    )
     num_classes = info.features["label"].num_classes
+    train_data, val_data = create_dataset(
+        args.batch_size_per_replica, datasets, strategy
+    )
 
-    train_data, val_data = get_dataset(batch_size_per_replica, datasets, strategy)
+    # train model
+    train(
+        args.n_epochs,
+        num_classes,
+        train_data,
+        val_data,
+        strategy,
+        verbose=convert_loglevel(args.loglevel),
+    )
 
-    strategy = tf.distribute.MirroredStrategy()
-
-    train(args.n_epochs, num_classes, train_data, val_data, strategy)
+    duration = round(time.perf_counter() - start_time, 4)
+    logger.info(f"Training completed in {duration}s.")
